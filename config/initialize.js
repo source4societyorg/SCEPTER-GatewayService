1bb1fa8bf696d614eb77b6b0eb51ed8f4aa2e50c
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-13 00:16:01 -0800

    Modified service name to use a common file

---
+++ config/initialize.js
@@ -0,0 +1,1 @@
+console.log('Be aware that this repository depends on a services.json configuration in your /config file. See the README there for more information')
--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict'
 const universalProxy = require('./handler.proxy').proxy
-const awsProxy = (event, context, callback) => (universalProxy(event, context, callback))()
-const azureProxy = (context, req) => (universalProxy(req, context, context.done))()
+const awsProxy = (event, context, callback) => universalProxy(event, context, callback)
+const azureProxy = (context, req) => universalProxy(req, context, context.done)
 switch(process.env.PROVIDER) {
   case 'azure':
     module.exports.proxy = azureProxy
