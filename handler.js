d12969a3d44a72df2e2832a3cc3b73aba7576776
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-14 23:07:41 -0800

    Achieved compatibility with AWS and Azure function providers

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 'use strict'
 const universalProxy = require('./handler.proxyRequest').proxyRequest
 const awsProxy = (event, context, callback) => universalProxy(event, context, callback)
-const azureProxy = (context, req) => universalProxy(req, context, context.done)
+const azureProxy = (context, req) => universalProxy(req, context, (err, res) => { context.done(err, res) })
 switch (process.env.PROVIDER) {
-  case 'azure':
+  case 'azure':    
     module.exports.proxyRequest = azureProxy
     break
   default:
